5debc0373295e3fa574c0d21e67a68befb0c4d7a
Mnemonical <mnemonical@pi.toern>
2013-09-27 15:56:43 +0200

    minor changes to the commentaries

--- GY_80_I2C_Gyro_Acc_Mag/GY_80_I2C_Gyro_Acc_Mag.ino
+++ GY_80_I2C_Gyro_Acc_Mag/GY_80_I2C_Gyro_Acc_Mag.ino
@@ -1,8 +1,8 @@
-#include <Wire.h>
+#include <Wire.h>              //include Wire.h library to allow communication over the I2C bus
 
 /***BEGIN L3G4200D***/
 
-int gyroAddress = 0x69;         //I2C address of the L3G4200D gyroscope
+int gyroAddress = 0x69;        //I2C address of the L3G4200D gyroscope
 
 #define GYRO_CTRL_REG1 0x20    //L3G4200D output data rate selection, bandwidth selection, set power down mode, enable x,y,z axis
 #define GYRO_CTRL_REG2 0x21    //L3G4200D high-pass-filter (HPF) and operations mode settings
