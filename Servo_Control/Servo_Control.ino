9d9d69f7e17ed94053bfe8fe3a38dfd0fbce64e9
Mnemonical <mnemonical@pi.toern>
2013-10-01 13:49:55 +0200

    control two servos via serial bus

--- Servo_Control/Servo_Control.ino
+++ Servo_Control/Servo_Control.ino
@@ -1,25 +1,46 @@
-#include <Servo.h>
+#include <Servo.h>            //include Servo.h library to allow easy control of the two servo motors
+#include <string.h>
+#include <Stream.h>
 
-Servo Sail;
+Servo Sail;                   //define Servo object for the servo motor controlling the sails
+Servo Rudder;                 //define Servo object for the servo motor controlling the rudder
 
 void setup()
 {
-  Sail.attach(7);
-  Sail.write(90);
-  Serial.begin(115200);
+  Sail.attach(7);             //tell the sail servo object which pin on the Arduino board the servos control line is connected to
+  Rudder.attach(6);
+  
+  Sail.write(90);             //center the servo position - position is given as int for the desired angle
+  Rudder.write(90);           //(position range should be roughly from 0° to 180°)
+  
+  Serial.begin(115200);      //start serial bus with given data rate
   Serial.println("Enter servo position in degrees between 0 and 180.");
+  Serial.println("s or r followed by the desired angle - example: s90 ");
 }
 
 void loop()
 { 
-  while (Serial.available())
+  while (Serial.available())              //check serial bus for incoming data
   {
-    int degree = Serial.parseInt();
-    Serial.println(degree);
-    Sail.write(degree);
-    Serial.println("Again?!");
-    delay (250);
+    parseServoString(Serial.readString());
   }
+  delay(500); 
 }
 
-
+void parseServoString(String degree)
+{ 
+  if (degree[0] == 's')
+  {
+    degree.replace("s","");
+    Sail.write(degree.toInt());
+  }
+  else if (degree[0] == 'r')
+  {
+    degree.replace("r","");
+    Rudder.write(degree.toInt());
+  }
+  else
+  {
+    Serial.println("illegal input");
+  }
+}
